101d68083da9e15503a3841e7bfced24e5a25181
Jack Ambery <jack.ambery@gmail.com>
2021-12-06 18:09:22 -0500

    organinzing for final product, set up config

--- NASAPicOfTheDay.py
+++ NASAPicOfTheDay.py
@@ -1,18 +1,18 @@
-# Final Project Checkpoint #4
-# CS 101: Checkpoint 4
-# Filename: checkpoint3and4.py
-#
+# Final Project for CS101 Intro to Computing
 # Name: John (Jack) Ambery
+# Filename: NASAPicOfTheDay.py
+#
 # Using a program to retrieve data from an API and implements a looping menu
-# This was built off the code of checkpoint 3
-# This will be the last time I use the NASA API for my CS Final Project
+#
+# This project is for Introduciton to Computing Final Project
 
 import json
 import requests
 import webbrowser
+import config
 
 def getData():
-    response = requests.get("https://api.nasa.gov/planetary/apod/?api_key=qGPdOISKXp8YG2vqUX5KK9WjwVlelOIFqwWl29fx")
+    response = requests.get("https://api.nasa.gov/planetary/apod/?api_key=" + config.API_KEY)
     data = json.loads(response.text)
     return data
 
@@ -35,15 +35,15 @@ def askForDate():
     day = input("What is the day you would like? (8th = 08, 23rd = 23)\n")
     month = input("What is the month you would like? (March = 03)\n")
     year = input("What is the year you would like?\n")
-    return month + "/" + month + "/" + year
+    return day + "/" + month + "/" + year
 
 #very basic menu
 def menu():
-    print("Select 1 for keys")
-    print("Select 2 for values")
-    print("Select 3 to open today's image of the day online")
-    print("Select 4 to see who took the image")
-    print("Select 5 to see the title of the image")
+    # print("Select 1 for keys")
+    # print("Select 2 for values")
+    print("Select 1 to open today's image of the day online")
+    print("Select 4 to see the details of today's image")
+    print("Select 3 to enter your own day")
     print("Select 0 to quit")
 
 def process(data):
---
+++ Picture.py
@@ -0,0 +1,6 @@
+class Picture:
+    def __init__(self):
+        pass
+
+    def __repr__(self) -> str:
+        pass
